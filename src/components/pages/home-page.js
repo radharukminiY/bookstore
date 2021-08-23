cf215e02aec2bf033135568937ed1f8488658360
radharukminiY <rukku558@gmail.com>
2021-08-23 20:52:19 +0530

    commit 6

---
+++ src/components/pages/home-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BookListContainer from "../../containers/book-list-container/book-list-container";
+import ShoppingCartTable from "../shopping-cart-table";
+
+const HomePage = () => {
+  return (
+    <div>
+      <BookListContainer />
+      <ShoppingCartTable />
+    </div>
+  );
+}
+
+export default HomePage;
