d6e66b682bd69c219d0cd9bbaa1e9c55d4e1a35f
radharukminiY <rukku558@gmail.com>
2021-08-28 23:14:35 +0530

    commit 10

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-pulse-wq4jmex0tor">
+      <div className="ldio-bq6z1k6dbpt">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
