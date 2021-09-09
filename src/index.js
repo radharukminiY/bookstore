bb4dfa2f8c2f3df5a156af58af97a79c2d5a8f54
radharukminiY <rukku558@gmail.com>
2021-09-09 22:38:17 +0530

    commit 15

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/app/app";
 
 import ErrorBoundry from "./components/error-boundry";
 import BookstoreService from "./services/bookstore-service";
-import {BookstoreServiceProvider} from "./components/bookstore-service-context";
+import {BookstoreServiceProvider} from "./components/bookstore-service";
 import {Provider} from "react-redux";
 import store from "./store";
 import {BrowserRouter as Router} from "react-router-dom";
